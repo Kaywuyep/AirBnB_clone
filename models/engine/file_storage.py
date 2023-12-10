e41809e4233bba861c041d54be0efc58c751f875
Kaywuyep <wuyepkate@gmail.com>
2023-12-10 11:36:27 -0800

    correctn

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """our storage engine"""
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -43,11 +44,33 @@ class FileStorage:
         with open(self.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
+    def class_list(self):
+        """Returns a dictionary of valid list of the classes
+        and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        class_list = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return class_list
+
     def reload(self):
         """
         Deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists; otherwise, do nothing)
         """
+         if not os.path.isfile(self.__file_path):
+            return
         try:
             with open(self.__file_path, 'r') as file:
                 loaded_objects = json.load(file)
