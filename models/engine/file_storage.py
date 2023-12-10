6e157e6ac6789e5385cef93ce1980a6207351608
Kaywuyep <wuyepkate@gmail.com>
2023-12-10 12:37:18 -0800

    new corrections

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,30 +44,11 @@ class FileStorage:
         with open(self.__file_path, 'w') as file:
             json.dump(serialized_objects, file)
 
-    def class_list(self):
-        """Returns a dictionary of valid list of the classes
-        and their references"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
-
-        class_list = {"BaseModel": BaseModel,
-                   "User": User,
-                   "State": State,
-                   "City": City,
-                   "Amenity": Amenity,
-                   "Place": Place,
-                   "Review": Review}
-        return class_list
-
     def reload(self):
         """
         Deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists; otherwise, do nothing)
+        """
         try:
             with open(self.__file_path, 'r') as file:
                 loaded_objects = json.load(file)
@@ -78,15 +59,6 @@ class FileStorage:
                 # Create instance using the dictionary
                 obj_instance = obj_class(**value)
                 self.__objects[key] = obj_instance
-        """
-        if not os.path.isfile(self.__file_path):
-            return
-
-        with open(self.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {key: self.class_list()[value["__class__"]](**value)
-                        for key, value in obj_dict.items()}
-            self.__objects = obj_dict
 
         except FileNotFoundError:
             pass  # If the file doesn't exist, do nothing
