d3455be4676b80e8de7e8cd8319032a5900286a3
Kaywuyep <wuyepkate@gmail.com>
2023-12-10 12:34:03 -0800

    new correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """our storage engine"""
 import json
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.place import Place
@@ -67,7 +68,6 @@ class FileStorage:
         """
         Deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists; otherwise, do nothing)
-        """
         try:
             with open(self.__file_path, 'r') as file:
                 loaded_objects = json.load(file)
@@ -78,6 +78,15 @@ class FileStorage:
                 # Create instance using the dictionary
                 obj_instance = obj_class(**value)
                 self.__objects[key] = obj_instance
+        """
+        if not os.path.isfile(self.__file_path):
+            return
+
+        with open(self.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {key: self.class_list()[value["__class__"]](**value)
+                        for key, value in obj_dict.items()}
+            self.__objects = obj_dict
 
         except FileNotFoundError:
             pass  # If the file doesn't exist, do nothing
